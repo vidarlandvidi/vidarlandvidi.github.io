129a8ba4a23eb889e1da0c3409a4c4432109d61a
Vidar Landvidi <mdd@smtp.ru>
2020-01-12 17:34:34 +0000

    adding debug logging

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const version = "0.0.01";
 const cacheName = `ikeychain-${version}`;
 self.addEventListener('install', e => {
+	console.debig('sw install');
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
@@ -13,10 +14,12 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', event => {
+	console.debig('sw activate');
   event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
+	console.debig('sw fecth');
   event.respondWith(
     caches.open(cacheName)
       .then(cache => cache.match(event.request, {ignoreSearch: true}))
