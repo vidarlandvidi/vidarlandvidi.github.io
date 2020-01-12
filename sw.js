4a786708d83d5764b2c90d55982c9be1e9687aaf
Vidar Landvidi <mdd@smtp.ru>
2020-01-12 17:38:39 +0000

    remove wrong debug logging

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 const version = "0.0.01";
 const cacheName = `ikeychain-${version}`;
 self.addEventListener('install', e => {
-	console.debig('sw install');
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
@@ -14,12 +13,10 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', event => {
-	console.debig('sw activate');
   event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-	console.debig('sw fecth');
   event.respondWith(
     caches.open(cacheName)
       .then(cache => cache.match(event.request, {ignoreSearch: true}))
